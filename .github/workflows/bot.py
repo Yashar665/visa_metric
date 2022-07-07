004339fe0f3ad8e6f6b510663199d26ea52c9b8b
Yashar665 <51043497+Yashar665@users.noreply.github.com>
2022-07-07 17:44:05 +0400

    Update bot.py

--- .github/workflows/bot.py
+++ .github/workflows/bot.py
@@ -21,7 +21,7 @@ display.start()
 while True:
     driver_path = ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install()
     driver = webdriver.Chrome(driver_path)
-    time.sleep(5)
+    driver.implicitly_wait(20)
     elem = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/div[1]/select")
     elem.send_keys("Azerbaijan")
     elem = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/div[2]/select")
